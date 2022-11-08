9a6a9a2c37ccf6e236c9dc84012a3ab3c6f01671
Kevin Allioli <kevin@linit.io>
2022-11-08 10:34:09 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,28 @@
+/*exports.database = {
+	type: 'mongodb',
+	hostname: 'localhost',
+	port: 27017,
+	database: 'scrumblr'
+};
+*/
+
+var argv = require('yargs')
+        .usage('Usage: $0 [--port INTEGER [8080]] [--baseurl STRING ["/"]] [--redis STRING:INT [127.0.0.1:6379]] [--gaEnabled] [--gaAccount STRING [UA-2069672-4]]')
+        .argv;
+
+exports.server = {
+	port: argv.port || 8080,
+	baseurl: argv.baseurl || '/'
+};
+
+exports.googleanalytics = {
+	enabled: argv['gaEnabled'] || false,
+	account: argv['gaAccount'] || "UA-2069672-4"
+};
+
+exports.database = {
+	type: 'redis',
+	prefix: '#scrumblr#',
+	redis: argv.redis || 'redis://127.0.0.1:6379'
+};
+
